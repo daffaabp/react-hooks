8045a1a6239142cea98cb9a29c795c9647dd0a77
daffaabp <daffa.budi2003@gmail.com>
2024-04-07 00:11:10 +0700

    useContext

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
+import "./App.css";
 // import UseState from './hooks-contoh/UseState'
 // import UseReducer from './hooks-contoh/UseReducer'
 // import UseEffect from './hooks-contoh/UseEffect'
-import UseLayoutEffect from './hooks-contoh/UseLayoutEffect'
-import "./App.css";
+// import UseLayoutEffect from "./hooks-contoh/UseLayoutEffect";
+import UseContext from "./hooks-contoh/UseContext";
 
 function App() {
-
-    return (
-        <div>
-            {/* <UseState />/ */}
-            {/* <UseReducer /> */}
-            {/* <UseEffect /> */}
-            <UseLayoutEffect />
-        </div>
-    )
+  return (
+    <div>
+      {/* <UseState />/ */}
+      {/* <UseReducer /> */}
+      {/* <UseEffect /> */}
+      {/* <UseLayoutEffect /> */}
+      <UseContext />/
+    </div>
+  );
 }
 
-export default App
+export default App;

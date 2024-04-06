39911da180503fb9e02e20be84991e9d16cd0791
daffaabp <daffa.budi2003@gmail.com>
2024-04-06 23:53:30 +0700

    useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 import './App.css'
 import UseState from './hooks-contoh/UseState'
-import UseReducer from './hooks-contoh/useReducer'
+import UseReducer from './hooks-contoh/UseReducer'
+import UseEffect from './hooks-contoh/UseEffect'
 
 function App() {
 
-return (
-<div>
-    {/*
-    <UseState />/ */}
-    <UseReducer />
-</div>
-)
+    return (
+        <div>
+            {/* <UseState />/ */}
+            {/* <UseReducer /> */}
+            <UseEffect />
+        </div>
+    )
 }
 
 export default App
